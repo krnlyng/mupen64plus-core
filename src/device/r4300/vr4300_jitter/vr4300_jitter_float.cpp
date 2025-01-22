a02fab339f08592eb2f444f07b1c34ee37e012b8
Franz-Josef Haider <f_haider@gmx.at>
2025-01-22 17:21:54 +0200

    Fix 32 bit float reg usage in CVT_L_S.

--- src/device/r4300/vr4300_jitter/vr4300_jitter_float.cpp
+++ src/device/r4300/vr4300_jitter/vr4300_jitter_float.cpp
@@ -1237,7 +1237,7 @@ void VR4300_Jitter::recompile_CVT_L_S(struct jit_instr *op)
     VALIDATE_FOUT(op, d);
 
     RCOpArg Rs = m_fpr.Use(op->s, RCMode::Read, true);
-    RCX64Reg Rd = m_fpr.Bind(op->d, RCMode::Write, true);
+    RCX64Reg Rd = m_fpr.Bind(op->d, RCMode::Write);
     RCX64Reg scratch2 = m_fpr.Scratch();
     RCOpArg gprscratch = m_gpr.Scratch();
     RegCache::Realize(Rs, Rd, scratch2, gprscratch);
