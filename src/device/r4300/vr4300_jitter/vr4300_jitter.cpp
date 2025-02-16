2dbaeda02191c9bd074cf2f6fbdcbd34b87a70d5
Franz-Josef Haider <f_haider@gmx.at>
2025-02-16 18:17:09 +0200

    Unimplemented operation ROUND_W_W.

--- src/device/r4300/vr4300_jitter/vr4300_jitter.cpp
+++ src/device/r4300/vr4300_jitter/vr4300_jitter.cpp
@@ -2099,6 +2099,7 @@ unsigned int VR4300_Jitter::Analyze(unsigned int addr, struct prepared_code_bloc
                 || instr->operation == VR4300_OP_CVT_D_D
                 || instr->operation == VR4300_OP_CVT_W_L
                 || instr->operation == VR4300_OP_CVT_L_W
+                || instr->operation == VR4300_OP_ROUND_W_W
                 || instr->operation == VR4300_OP_BC3F
                 || instr->operation == VR4300_OP_BC3FL
                 || instr->operation == VR4300_OP_BC3T
@@ -4794,6 +4795,7 @@ void VR4300_Jitter::recompile_instruction(struct jit_instr *op)
             case VR4300_OP_CVT_D_D:
             case VR4300_OP_CVT_W_L:
             case VR4300_OP_CVT_L_W:
+            case VR4300_OP_ROUND_W_W:
                 recompile_UNIMPLEMENTED_COP1(op);
                 break;
             default:
